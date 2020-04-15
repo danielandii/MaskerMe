6630eecfcf4e6b2eaad4ec4c44ea2b4862be917a
nore <noertjahja@yahoo.com>
2020-04-15 15:32:06 +0700

    Nambah pergantian screne (NORE)

---
+++ Assets/Scenes/SceneSwitcher.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    public void GotoNorePlay()
+    {
+        SceneManager.LoadScene("NorePlay");
+    }
+
+    public void GotoNoreMenu()
+    {
+        SceneManager.LoadScene("NoreMenu");
+    }
+
+    public void GotoNoreGameOver()
+    {
+        SceneManager.LoadScene("NoreGameOver");
+    }
+}

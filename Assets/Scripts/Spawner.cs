1b3900d5bcd483ee86f2c0e093b7ea6c71069d1a
Agyan Atma <agyanatma@MacBook-Air.local>
2020-04-21 09:30:59 +0700

    update speed & gravity

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -8,14 +8,24 @@ public class Spawner : MonoBehaviour
 
     private float timeSpawn;
     public float startTimeSpawn;
+    public float decreaseTimeSpawn;
+    public float minimumTime = 1.5f;
 
-    private void Update(){
-        if(timeSpawn <= 0){
+    private void Update()
+    {
+        if (timeSpawn <= 0)
+        {
             int i = Random.Range(0, obstacles.Length);
             Instantiate(obstacles[i], transform.position, Quaternion.identity);
             timeSpawn = startTimeSpawn;
+
+            if (startTimeSpawn > minimumTime)
+            {
+                startTimeSpawn -= decreaseTimeSpawn;
+            }
         }
-        else{
+        else
+        {
             timeSpawn -= Time.deltaTime;
         }
     }
